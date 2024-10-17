5423304241e86af873899e41a1b1e12b4b628789
Rixsitilla008 <aufauf833@gmail.com>
2024-10-17 08:30:05 +0500

    rasm ozgardi

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-// import img from "/public/me.dev.jpg"; // Bu qatorni o'chirib tashlang
 
 const Hero = () => {
     const { t } = useTranslation("hero");
-    const imgSrc = "/me.dev.jpg"; // Rasmning to'g'ridan-to'g'ri URLi
+    const imgSrc = "/me.dev.jpg"; 
 
     return (
         <section className='bg-oq dark:bg-dakr py-[50px]'>
             <div className='container flex lg:flex-row flex-col-reverse items-center justify-between gap-4'>
                 <div className='flex flex-col lg:items-start items-center lg:text-start text-center gap-[20px] sm:w-[50%]'>
                     <h1 className='text-qora dark:text-oq sm:text-5xl text-3xl font-bold'>{t("English")}</h1>
-                    <img src={imgSrc} alt="My photo" className='flex sm:hidden w-[300px] h-[300px] sm:w-[500px] sm:h-[500px] object-cover rounded-[50%]' />
+                    <img src={imgSrc} alt="My photo" className='flex sm:hidden w-[250px] h-[250px] sm:w-[500px] sm:h-[500px] object-cover rounded-[50%]' />
                     <span className='sm:text-xl text-lg text-gren cursor-pointer'>{t("Teach")}</span>
                     <ul className='flex flex-row sm:justify-start justify-center items-center text-qora dark:text-oq gap-3 text-2xl sm:text-4xl'>
                         <li>
